74226f2316fd24a44f748dc02c48e55f7fa8f901
Rahul Batra <rahul-primary-laptop@rahulbatra.in>
2019-09-23 13:07:58 +0530

    added countdown

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$('.fa-instagram').click(function() {
+        $(location).attr('href', 'https://instagram.com/equilibria_2019');
+    })
+    //Date countdown
+function getTime() {
+    var today = new Date();
+    var yearToLive = 2019;
+    var currentYear = today.getFullYear();
+    var diffToYear = currentYear - yearToLive;
+    var currentMonth = today.getMonth();
+    var currentDay = today.getDate();
+    var daysToLive = 30;
+    var diffToDays = daysToLive - currentDay;
+    const monthToLive = 10;
+    var diffToMonth = monthToLive - currentMonth;
+    var currentHours = today.getHours();
+    var hoursToLive = 00;
+    var diffToHours = currentHours - hoursToLive;
+    var currentMinutes = today.getMinutes();
+    var minutesToLive = 60;
+    var diffToMinutes = minutesToLive - currentMinutes;
+    var currentSeconds = today.getSeconds();
+    var secondsToLive = 60;
+    var diffToSeconds = secondsToLive - currentSeconds;
+    var TimeDifference = diffToYear + ' Years ' + diffToMonth + ' Months ' + diffToDays + ' Days ' + diffToHours + ' Hours ' + diffToMinutes + ' Minutes ' + diffToSeconds + ' Seconds ';
+    $(".countdown").text(TimeDifference);
+    var t = setTimeout(getTime, 500);
+
+}
+
+
+getTime();
